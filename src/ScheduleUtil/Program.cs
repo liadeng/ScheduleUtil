e68f7b6364a6d03909f01ab6346e7227d74d5e44
liadeng <liadeng@cs.fiu.edu>
2019-09-26 13:32:05 -0400

    Project created

---
+++ src/ScheduleUtil/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ScheduleUtil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // this schedule will be active during 2000/01/01 and 9999/12/31, every day during 7:30 and 18:00
+            var dailyScheduleExample = new Schedule
+            {
+                StartDate = 20000101,
+                EndDate = 99991231,
+                Mode = 0,
+                DailyStart = 730,
+                DailyEnd = 1800,
+            };
+
+            // this schedule will be active during 2019/09/01 and 2020/10/01, every week on these days/time_ranges: TUE 2:00-4:00 TUE 6:00-18:00 TUE 20:00-22:00 THU 0:00-24:00 FRI 8:00-20:00
+            var weeklyScheduleExample = new Schedule
+            {
+                StartDate = 20190901,
+                EndDate = 20201001,
+                Mode = 1,
+                WeeklyString = "[[],[{\"start\":\"200\",\"end\":\"400\"},{\"start\":\"600\",\"end\":\"1800\"},{\"start\":\"2000\",\"end\":\"2200\"}],[],[{\"start\":\"0\",\"end\":\"2400\"}],[{\"start\":\"800\",\"end\":\"2000\"}],[],[]]",
+            };
+
+            // active every month on day 2, 3, 7, 9 from 8:00 to 18:30
+            var monthlyScheduleExample = new Schedule
+            {
+                StartDate = 20190901,
+                EndDate = 20201001,
+                Mode = 2,
+                MonthlyString = "[2,3,7,9]",
+                MonthlyStart = 800,
+                MonthlyEnd = 1830,
+            };
+
+            bool isActive = IsScheduleActiveAtGivenTime(weeklyScheduleExample, 1569511207, "Eastern Standard Time");
+        }
+
+        struct UnixTimestampRange
+        {
+            long start;
+            long end;
+        }
+
+        struct RepeatableTimeRange
+        {
+            long baseTimestamp;
+            long duration;
+            long repeatInterval;
+            long cutoffTimestamp;
+        }
+
+        /// <summary>
+        /// Please implement this function, which returns if a schedule is active at a certain local time
+        /// You need to convert unixTimestamp/timeZoneText to local time, 
+        ///     then extract year/month/day/hour/minute info from the local time object,
+        ///     and finally check if the schedule is active at this time
+        /// </summary>
+        private static bool IsScheduleActiveAtGivenTime(Schedule schedule, long unixTimestamp, string timeZoneText)
+        {
+            TimeZoneInfo timeZone = TimeZoneInfo.FindSystemTimeZoneById(timeZoneText);
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Please implement this function, which returns a list of Daylight Saving Time (DST) ranges, in given year range
+        ///     Range.start = when DST starts (in the form of unixTimestamp)
+        ///     Range.end = when DST ends
+        /// For each year, you need to find out when DST starts/ends at the given timezone,
+        ///     then convert that time to unixTimestamp
+        /// </summary>
+        private static List<UnixTimestampRange> GetListOfDST(string timeZoneText, int yearStart, int yearEnd)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Please implement this function, which converts a schedule in a certain timezone to a list of equivalent repeatable time ranges.
+        /// A RepeatableTimeRange defines a time range that repeats itself for a definite number of times:
+        ///     baseTimestamp: the first unixTimestamp when the schedule is active
+        ///     duration: the schedule is active until baseTimestamp + duration
+        ///     repeatInterval: the schedule is active again from baseTimestamp + repeatInterval, to baseTimestamp + repeatInterval + duration, and keep repeating itself
+        ///     cutoffTimestamp: the schedule is inactive forever after this time
+        /// yearStart & yearEnd: the list of RepeatableTimeRange should be covering these years
+        /// Important notes: treat all timezone as if they don't have DST (otherwise it will be really complicated, because repeatable time ranges will be wrong when crossing DST start/end date)
+        /// For example, a daily schedule will yield a list of only one element (for which repeatInterval = 3600*24)
+        ///     Another example, a weekly schedule will yield a list of several elements, depending on how many time ranges are there in the schedule.
+        ///     A monthly schedule isn't really repeatable because each month have different days. And it's not even repeatable on a yearly scale because, there are leap years.
+        ///         So, for a monthly schedule, you need to generate one time range for each of the month, and set repeatInterval = duration, cutoffTimestamp = baseTimestamp + duration
+        /// </summary>
+        private static List<RepeatableTimeRange> ConvertScheduleToTimeRanges(Schedule schedule, string timeZoneText, int yearStart, int yearEnd)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/ScheduleUtil/Schedule.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ScheduleUtil
+{
+    /// <summary>
+    /// Schedules are TimeZone independent, it must always be used together with a TimeZone to be specific.
+    /// It's like when you tell your cellphone "Wake me up tomorrow at 7:00 AM", the cellphone will wake you up at 7 in your TimeZone.
+    /// If the timezone information is not given, there is no way for the cellphone to know when to trip the alarm
+    /// </summary>
+    class Schedule
+    {
+        /// <summary>
+        /// Usage: The schedule will only be active during this date range
+        /// Encoded as year * 10000 + month * 100 + day, for example, 2019/09/26 = 20190926
+        /// </summary>
+        public int StartDate { get; set; }
+
+        public int EndDate { get; set; }
+
+        /// <summary>
+        /// Usage: decide how the schedule repeats.
+        /// Mode == 0 means it repeats Daily
+        /// Mode == 1 means it repeats Weekly
+        /// Mode == 2 means it repeats Monthly
+        /// </summary>
+        public int Mode { get; set; }
+
+        /// <summary>
+        /// Usage: only used when Mode == 0, the schedule will only be active during this time range
+        /// Encoded as hour * 100 + miniute, for example, 14:30 = 1430
+        /// </summary>
+        public int DailyStart { get; set; }
+
+        public int DailyEnd { get; set; }
+
+        /// <summary>
+        /// Usage: only used when Mode == 1, the schedule will only be active during centain days/time_range of every week
+        /// Format: will be formatted as a json string like this: [[{"start":0730,"end":1800}, {}, ...], [], [], ...]
+        ///     The outter most array has 7 elements, representing each day of the week (the first element is Monday)
+        ///         Each element is an array of {start,end} objects, each of the object represents a time range in that day of week that the schedule will be active
+        /// </summary>
+        public string WeeklyString { get; set; }
+
+        /// <summary>
+        /// Usage: only used when Mode == 2, the schedule will only be active on these days of every month
+        /// Format: will be formatted as a json string like this: [1,2,3,...,31]
+        /// </summary>
+        public string MonthlyString { get; set; }
+
+        /// <summary>
+        /// Usage: only used when Mode == 2, the schedule will only be active during this time range
+        /// </summary>
+        public int MonthlyStart { get; set; }
+
+        public int MonthlyEnd { get; set; }
+    }
+}
